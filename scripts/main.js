f3eb9ccd306f02a91ffaaa07a33c65b0aa5d7dc5
Raquel Bonilla <raquelb.2014@gmail.com>
2017-03-20 19:23:43 -0700

    table now only prints first 4 parts of employee data

--- scripts/main.js
+++ scripts/main.js
@@ -16,38 +16,45 @@ function preload(){
 
 
 
-//all of the below is a working table for the json data above
 
 // Builds the HTML Table out of myList.
-function buildHtmlTable(selector) {
+function buildTable(selector) {
   var columns = addAllColumnHeaders(myList.employee, selector);
 
   for (var i = 0; i < myList.employee.length; i++) {
-    var row$ = $('<tr/>');
-    for (var colIndex = 0; colIndex < columns.length; colIndex++) {
-      var cellValue = myList.employee[i][columns[colIndex]];
-      if (cellValue == null) cellValue = "";
-      row$.append($('<td/>').html(cellValue));
+    var row = $('<tr/>');
+    for (var j = 0; j < 4; j++) {
+      var cellValue = myList.employee[i][columns[j]];
+      //if (cellValue == null) cellValue = "";
+      row.append($('<td/>').html(cellValue));
     }
-    $(selector).append(row$);
+    $(selector).append(row);
   }
 }
 
 // Adds a header row to the table and returns the set of columns.
 // Need to do union of keys from all records as some records may not contain
 // all records.
+
+
 function addAllColumnHeaders(myList, selector) {
   var columnSet = [];
   var headerTr$ = $('<tr/>');
+  var count = 0;
 
-  for (var i = 0; i < myList.length; i++) {
+  for (var i = 0; i < myList.length && (count != 4) ; i++) {
     var rowHash = myList[i];
     for (var key in rowHash) {
       if ($.inArray(key, columnSet) == -1) {
         columnSet.push(key);
         headerTr$.append($('<th/>').html(key));
+        count++;
+        
       }
+      if (count == 4)
+        break;
     }
+    
   }
   $(selector).append(headerTr$);
 
