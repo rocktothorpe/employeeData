f6f09eb944a270c71699337ddf45151fcbd28e00
Raquel Bonilla <raquelb.2014@gmail.com>
2017-03-23 01:10:50 -0700

    added tbody property to the table, enabling bootstrap to work as it should

--- js/script.js
+++ js/script.js
@@ -15,21 +15,44 @@ function preload(){
 */
 
 
+//different attempt to build a dynamic table
+/*
 
+function buildTable(selector) {
+
+  //build table head
+
+
+}
+
+
+
+*/
+
+
+
+
+///*
 
 // Builds the HTML Table out of empData.
 function buildTable(selector) { //the selector is empDataTable
   var columns = addAllColumnHeaders(empData.employee, selector);
-
+  var tableBody = $('<tbody>');
+  
   for (var i = 0; i < empData.employee.length; i++) {
-    var row = $('<tr/>');
+    var row = $('<tr>');
+    tableBody.append(row);
     for (var j = 0; j < 4; j++) {
-      var cellValue = empData.employee[i][columns[j]];
-      row.append($('<td/>').html(cellValue));
-    }
-    $(selector).append(row);
+      var cellValue = empData.employee[i][columns[j]]; //gives cell value column data for the row
+      row.append($('<td>').html(cellValue));//adds table data to the row
+    }//finished adding columns to the row
+    
+    tableBody.append(row);//adds row to the body
+
   }
-   
+   $(selector).append(tableBody);
+
+
 }
 
 // Adds a header row to the table and returns the set of columns.
@@ -37,9 +60,11 @@ function buildTable(selector) { //the selector is empDataTable
 // all records.
 
 
+
 function addAllColumnHeaders(empData, selector) {
   var columnSet = [];
-  var headerTr$ = $('<tr/>');
+  var rowHead$ = $('<tr>');
+  var header$ = $('<thead>');
   var count = 0;
   var colNum = 4;
 
@@ -48,15 +73,15 @@ function addAllColumnHeaders(empData, selector) {
     for (var key in rowHash) {
       if ($.inArray(key, columnSet) == -1) {
         columnSet.push(key);
-        headerTr$.append($('<th/>').html(key));
+        rowHead$.append($('<th>').html(key));
         count++;
       }
       if (count == 4)
         break;
     }
-    
+    header$.append(rowHead$)
   }
-  $(selector).append(headerTr$);
+  $(selector).append(header$);
 
   return columnSet;
 }
