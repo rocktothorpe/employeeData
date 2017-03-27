b3fbc251c96a2d07e05e0bfe13b9d65c94b2b5f4
Raquel Bonilla <raquelb.2014@gmail.com>
2017-03-26 20:36:13 -0700

    rows are now clickableand employee data appears at the bottom of the table

--- js/script.js
+++ js/script.js
@@ -2,46 +2,27 @@
 
 var empData;
 
+//stores json data into empData
 $.getJSON('data.json', function(Data){
        empData = Data;
-       //alert(empData.property);
+       
  });
 
 
-/*
-function preload(){
-  empData = loadJSON("data.json");
-}
-*/
-
-
-//different attempt to build a dynamic table
-/*
-
-function buildTable(selector) {
-
-  //build table head
-
-
-}
-
-
-
-*/
-
-
-
-
-///*
 
 // Builds the HTML Table out of empData.
 function buildTable(selector) { //the selector is empDataTable
   var columns = addAllColumnHeaders(empData.employee, selector);
+
+  //var columns = ["First Name", "Last Name", "Position", "Salary"];
   var tableBody = $('<tbody>');
   
   for (var i = 0; i < empData.employee.length; i++) {
-    var row = $('<tr>');
+    //var row = $('<tr class = "row' + i + '">');//gives each row a unique class name
+    var row = $('<tr id= "' + i + '"  data-target="#popUpData" data-toggle="collapse" onclick="content(this)" >');
+
     tableBody.append(row);
+    //tableBody.append(row);
     for (var j = 0; j < 4; j++) {
       var cellValue = empData.employee[i][columns[j]]; //gives cell value column data for the row
       row.append($('<td>').html(cellValue));//adds table data to the row
@@ -54,6 +35,8 @@ function buildTable(selector) { //the selector is empDataTable
 
 
 }
+
+
 
 // Adds a header row to the table and returns the set of columns.
 // Need to do union of keys from all records as some records may not contain
@@ -85,6 +68,59 @@ function addAllColumnHeaders(empData, selector) {
 
   return columnSet;
 }
+
+
+/*
+$(document).ready(function() {
+    $('#empDataTable').DataTable( {
+        data: empData.employee,
+        columns: [
+            { title: "First Name" },
+            { title: "Last Name" },
+            { title: "Position" },
+            { title: "Salary" }
+        ]
+    } );
+} );
+*/
+
+/*
+
+        <div id="popUpData" class="collapse in">
+        <p id = "name">Name: <br></br></p>
+        <p id = "pos">Postiion: <br></br></p>
+<p id = "phone">Phone: <br></br></p>
+<p id = "email">Email: <br></br></p>
+<p id = "office">Office: <br></br></p>
+*/
+
+function content(elem) 
+{
+    var destination = document.getElementById("popUpData"); //where the data should go
+    var id = elem.id; 
+    var idNum = Number(id);//changes id from string to number for array access
+    var name = empData.employee[idNum]["First Name"] + " " + empData.employee[idNum]["Last Name"];
+    //alert(name);
+    //var pos = empData.employee[idNum].Postion;
+    //var phone = empData.employee[idNum].Phone;
+    //var email = empData.employee[idNum].Email;
+    //var office = empData.employee[idNum].Office;
+    var p1 = document.getElementById("name");
+    p1.innerHTML = 'Name: ' + empData.employee[idNum]["First Name"] + " " + empData.employee[idNum]["Last Name"];
+    var p2 = document.getElementById("pos");
+    p2.innerHTML = 'Position: ' + empData.employee[idNum].Position;
+    var p3 = document.getElementById("phone");
+    p3.innerHTML = 'Phone: ' + empData.employee[idNum].Phone;
+    var p4 = document.getElementById("email");
+    p4.innerHTML = 'Email: ' + empData.employee[idNum].Email;
+    var p5 = document.getElementById("office");
+    p5.innerHTML = 'Office: ' + empData.employee[idNum].Office;
+    
+    //destination.append(list).html(name);
+}
+
+
+
 
 
 
